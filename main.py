bd77cb1021e359c73085bca3aa2fdbfd413e56f6
Maxime <maxime.veysseyre@gmail.com>
2020-11-16 09:37:56 +0100

    [UPDATE] Modifications mineures

--- main.py
+++ main.py
@@ -28,9 +28,6 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Conv2D(28, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# model.add(Conv2D(28, (3, 3), activation="relu"))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dense(units=128, activation="relu"))
 model.add(Dense(units=26, activation="softmax"))
@@ -40,63 +37,6 @@ model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accur
 model.summary()
 
 entrainement = model.fit_generator(train_generator, steps_per_epoch=32, epochs=20, validation_data=test_generator, validation_steps=32)
-
-# pickle.dump(model, open("CNN_model.sav", "wb"))
-# model = pickle.load(open("CNN_model.sav","rb"))
-
-def get_result(result):
-    if result[0][0] == 1:
-        return("A")
-    elif result[0][0] == 2:
-        return ("B")
-    elif result[0][0] == 3:
-        return ("C")
-    elif result[0][0] == 4:
-        return ("D")
-    elif result[0][0] == 5:
-        return ("E")
-    elif result[0][0] == 6:
-        return ("F")
-    elif result[0][0] == 7:
-        return ("G")
-    elif result[0][0] == 8:
-        return ("H")
-    elif result[0][0] == 9:
-        return ("I")
-    elif result[0][0] == 10:
-        return ("J")
-    elif result[0][0] == 11:
-        return ("K")
-    elif result[0][0] == 12:
-        return ("L")
-    elif result[0][0] == 13:
-        return ("M")
-    elif result[0][0] == 14:
-        return ("N")
-    elif result[0][0] == 15:
-        return ("O")
-    elif result[0][0] == 16:
-        return ("P")
-    elif result[0][0] == 17:
-        return ("Q")
-    elif result[0][0] == 18:
-        return ("R")
-    elif result[0][0] == 19:
-        return ("S")
-    elif result[0][0] == 20:
-        return ("T")
-    elif result[0][0] == 21:
-        return ("U")
-    elif result[0][0] == 22:
-        return ("V")
-    elif result[0][0] == 23:
-        return ("W")
-    elif result[0][0] == 24:
-        return ("X")
-    elif result[0][0] == 25:
-        return ("Y")
-    elif result[0][0] == 26:
-        return ("Z")
 
 
 choix = r'data/Testing/a/A-200.png'
